24b550d320bf2747b01f3c33e35d137e67abd31e
Sanjaikumar-sk <pajanysanjaikumar@gmail.com>
2025-02-20 12:44:10 +0530

    Practice till objects i.e. JSON

--- Nodejs_practice/hello.js
+++ Nodejs_practice/hello.js
@@ -1,8 +1,22 @@
-var http = require('http')
 
-http.createServer(function (req,res)
-{
-    res.writeHead(200, {'Context-Type':'text/html'})
-    res.write("Hellow world!!!....")
-    res.end()
-}).listen(8080)
+let object = {
+    obj1 : "Sanjai",
+    obj2 : "Aadya",
+    obj3 : {
+        manager1 : "kaushik",
+        manager2 : "gopal",
+        manager3 : "Harish"
+    },
+    SD : {
+        SD1 : "Kaviya",
+        SD2 : "Arun",
+        SD3 : "Abirami"
+    },
+}
+
+console.log(object);
+console.log(object.obj2);
+console.log(object.obj3.manager3);
+delete object.obj3.manager3;
+console.log(object)
+
